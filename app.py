ed7c602fde0e56091e961773f4e7fbe6f07f2749
Kevin McCarthy <me@kevinmccarthy.org>
2015-08-20 11:41:59 -1000

    change passive port range

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def upload_to_s3(filename):
     s3.Object(BUCKET_NAME, os.path.basename(filename)).put(Body=open(filename, 'rb'))
 
 class S3FtpUploadHandler(FTPHandler):
-    passive_ports = range(5000, 5010)
+    passive_ports = range(10100, 10110)
     def on_file_received(self, filename):
         upload_to_s3(filename)
 
